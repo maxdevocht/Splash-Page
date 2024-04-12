1879ea0a58c372379363b523e4072b91c31a8d25
Max de Vocht <maxdevocht@hotmail.com>
2024-04-12 12:01:45 +0200

    Intro Styles

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Intro() {
     return (
         <section className="intro">
-            <h1>Turn <strong>your picture</strong> into an iconic <strong>Scrimba avatar</strong></h1>
+            <h1>Turn <strong className="strong">your picture</strong> into an iconic <strong className="strong">Scrimba avatar</strong></h1>
             <span class="beta-text">Currently in Beta</span>
         </section>
     )
