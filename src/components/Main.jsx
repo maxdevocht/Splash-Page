765f1cd46b7a9a84e054faa55c390a5903b550d6
Max de Vocht <maxdevocht@hotmail.com>
2024-04-12 11:56:21 +0200

    Added main styles

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,11 @@ import React from "react";
 export default function Main() {
     return (
         <main className="main-content">
-            <img src="./src/assets/images/avatar.png" alt="" className="main-img" />
+            <img src="./src/assets/images/avatar.png" alt="Scrimba avatar" className="main-img" />
+            <h2>Scrimbafy.me</h2>
+            <span className="subheading">Liven up your profile pic</span>
+            
+            <p>The colorful and iconic Scrimba avatars are no longer just for members of the Scrimba team and its Avengers-caliber group of code reviewers. You can Scrimbafy your photo today!</p>
         </main>
     )
 }
